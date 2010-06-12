2ec7315d43224728ac174276965932ef524d667d
FrancoisAncel <FrancoisAncel@localhost>
2010-06-12 10:22:16 +0200

    Abstract dll/so Library

---
+++ amv_core/WSharedLib.h
@@ -0,0 +1,17 @@
+
+#ifndef WSHAREDLIB_H_
+# define WSHAREDLIB_H_
+
+#include "ASharedLib.h"
+
+class WSharedLib : public ASharedLib
+{
+public:
+	virtual ~WSharedLib();
+	ASharedLib::lib_handler	LoadLib(std::string);
+	void* getFunc(ASharedLib::lib_handler&, std::string);
+	bool closeLib(ASharedLib::lib_handler&);
+	void openLibFrom(std::string dir, std::vector<ASharedLib::lib_handler>& tab);
+};
+
+#endif
