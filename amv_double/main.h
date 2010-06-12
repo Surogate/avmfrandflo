55c60eba9d0189a7727719890840e23bd7f50603
Florian Chanioux <Florian Chanioux@localhost>
2010-06-12 14:17:14 +0200

    dll : manque l'implementation des calcul

--- amv_double/main.h
+++ amv_double/main.h
@@ -2,27 +2,17 @@
 #define __MAIN_H__
 
 #include <windows.h>
+#include "AObj.h"
 
 /*  To use this exported function of dll, include this header
  *  in your project.
  */
 
-#ifdef BUILD_DLL
-    #define DLL_EXPORT __declspec(dllexport)
-#else
-    #define DLL_EXPORT __declspec(dllimport)
-#endif
+#define DLL_EXPORT __declspec(dllexport)
 
-
-#ifdef __cplusplus
 extern "C"
 {
-#endif
-
-void DLL_EXPORT SomeFunction(const LPCSTR sometext);
-
-#ifdef __cplusplus
+DLL_EXPORT AObj* getInstance();
 }
-#endif
 
 #endif // __MAIN_H__
--- amv_int16/avm_elem.cpp
+++ amv_int16/avm_elem.cpp
@@ -1,64 +1,65 @@
-#include "avm_double.h"
+#include <sstream>
+#include "avm_elem.h"
 
-avm_double::avm_double(void): _type("double"),
+avm_elem::avm_elem(void): _type("int16"),
 		                      _value(),
 							  _valuestr()
-{
-}
-
-avm_double::~avm_double(void)
 {}
 
-IObject*  avm_double::Add(const IOperand &object)
+avm_elem::~avm_elem(void)
+{}
+
+IObject*  avm_elem::Add(const IOperand &object)
 {
 return (0);
 }
 
-IObject*  avm_double::Subtract(const IOperand &object)
+IObject*  avm_elem::Subtract(const IOperand &object)
 {
 return (0);
 }
 
-IObject*  avm_double::Multiply(const IOperand &object)
+IObject*  avm_elem::Multiply(const IOperand &object)
 {
 return (0);
 }
 
-IObject*  avm_double::Divide(const IOperand &object)
+IObject*  avm_elem::Divide(const IOperand &object)
 {
 return (0);
 }
 
-void avm_double::setObj(std::string const &val)
+void avm_elem::setObj(std::string const &val)
 {
   this->_valuestr = val;
-  this->_value = std::atof(val.c_str());
+  std::istringstream iss( val );
+  iss >> this->_value;
 }
 
-AObj* avm_double::newClone() const
+AObj* avm_elem::newClone() const
 {
-  AObj * res = new avm_double;
+  AObj * res = new avm_elem;
   res->setObj(this->_valuestr);
   return res;
 }
 
-const std::string& avm_double::ToString() const
+const std::string& avm_elem::ToString() const
 {
   return (this->_valuestr);
 }
 
-const std::string& avm_double::GetType() const
+const std::string& avm_elem::GetType() const
 {
   return (this->_type);
 }
 
-bool avm_double::Equals(const IObject &value) const
+bool avm_elem::Equals(const IObject &value) const
 {
   return(this->ToString() == value.ToString());
 }
 
-IObject* avm_double::Clone() const
+IObject* avm_elem::Clone() const
 {
-  IObject* res = new avm_double;
+  IObject* res = new avm_elem;
   return res;
 }
