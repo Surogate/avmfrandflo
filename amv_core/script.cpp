1bd1cad24f2c58fed06823f2761be5c5ee309dd6
FrancoisAncel <FrancoisAncel@localhost>
2010-06-12 17:01:20 +0200

    implementation finished

--- amv_core/script.cpp
+++ amv_core/script.cpp
@@ -10,6 +10,15 @@ Script::Script(ObjFactory& factory, std::string dir)
         std::cerr << "Can't open file " << dir << std::endl;
         error_ = Script::invalid_file;
     }
+
+    func_tab["push"] = &Script::do_push;
+    func_tab["add"] = &Script::do_add;
+    func_tab["mul"] = &Script::do_mul;
+    func_tab["dump"] = &Script::do_dump;
+    func_tab["pop"] = &Script::do_pop;
+    func_tab["assert"] = &Script::do_assert;
+    func_tab["div"] = &Script::do_div;
+    func_tab["sub"] = &Script::do_sub;
 }
 
 Script::~Script()
@@ -22,26 +31,29 @@ void Script::clean(std::string&)
 
 }
 
-bool Script::getNextInstruction(Instruction& instru)
+bool Script::getNextInstruction()
 {
     if (ifs_.good())
     {
-        getline(ifs_, instru.instr);
-        unsigned int pos = std::string::npos;
-        if ((pos = instru.instr.find(";")) != std::string::npos)
+        getline(ifs_, instru_.instr);
+        if (ifs_.good())
         {
-            instru.instr.erase(pos);
-        }
-        pos  = instru.instr.find_first_of(" ");
-        instru.bytecode = instru.instr.substr(0, pos);
-        if (pos != std::string::npos)
-        {
-            unsigned int pos2  = instru.instr.find_first_of("(");
-            instru.argument = instru.instr.substr(pos + 1, pos2);
-            if (pos2 != std::string::npos)
+            unsigned int pos = std::string::npos;
+            if ((pos = instru_.instr.find(";")) != std::string::npos)
             {
-                unsigned int pos3 = instru.instr.find_first_of(")");
-                instru.value = instru.instr.substr(pos2 + 1, pos3);
+                instru_.instr.erase(pos);
+            }
+            pos  = instru_.instr.find_first_of(" ");
+            instru_.bytecode = instru_.instr.substr(0, pos);
+            if (pos != std::string::npos)
+            {
+                unsigned int pos2  = instru_.instr.find_first_of("(");
+                instru_.argument = instru_.instr.substr(pos + 1, pos2);
+                if (pos2 != std::string::npos)
+                {
+                    unsigned int pos3 = instru_.instr.find_first_of(")");
+                    instru_.value = instru_.instr.substr(pos2 + 1, pos3);
+                }
             }
         }
     }
@@ -57,11 +69,9 @@ void Script::run()
 {
     if (error_)
         return ;
-    Instruction instru_;
-
-    while(getNextInstruction(instru_) && !error_)
+    while(getNextInstruction() && !error_)
     {
-
+        execute();
     }
     if (error_)
     {
@@ -70,5 +80,134 @@ void Script::run()
     else
     {
         std::cerr << "Script ok" << std::endl;
+    }
+}
+
+void Script::execute()
+{
+    std::map< std::string, func_ptr >::const_iterator it = func_tab.find(instru_.bytecode);
+
+    if (it != func_tab.end())
+    {
+        (this->*func_tab[instru_.bytecode])();
+    }
+    else
+    {
+        error_ = invalid_bytecode;
+    }
+}
+
+void Script::do_push()
+{
+    AObj* arg = factory_.creat(instru_.argument, instru_.value);
+    if (arg)
+    {
+        stack_.push(arg);
+    }
+    else
+    {
+        error_ = unknow_type;
+    }
+}
+
+void Script::do_add()
+{
+    if (stack_.size() < 2)
+    {
+        error_ = not_enough_object;
+    }
+    else
+    {
+        IOperand* ope1 = stack_.top();
+        stack_.pop();
+        IOperand* ope2 = stack_.top();
+        stack_.pop();
+        stack_.push(static_cast< AObj* >(ope1->Add(*ope2)));
+    }
+}
+
+void Script::do_mul()
+{
+    if (stack_.size() < 2)
+    {
+        error_ = not_enough_object;
+    }
+    else
+    {
+        IOperand* ope1 = stack_.top();
+        stack_.pop();
+        IOperand* ope2 = stack_.top();
+        stack_.pop();
+        stack_.push(static_cast< AObj* >(ope1->Multiply(*ope2)));
+    }
+}
+
+void Script::do_div()
+{
+    if (stack_.size() < 2)
+    {
+        error_ = not_enough_object;
+    }
+    else
+    {
+        IOperand* ope1 = stack_.top();
+        stack_.pop();
+        IOperand* ope2 = stack_.top();
+        stack_.pop();
+        AObj* obj = static_cast< AObj* >(ope1->Divide(*ope2));
+        if (obj)
+        {
+            stack_.push(obj);
+        }
+        else
+        {
+            error_ = divide_by_zero;
+        }
+    }
+}
+
+void Script::do_sub()
+{
+    if (stack_.size() < 2)
+    {
+        error_ = not_enough_object;
+    }
+    else
+    {
+        IOperand* ope1 = stack_.top();
+        stack_.pop();
+        IOperand* ope2 = stack_.top();
+        stack_.pop();
+        stack_.push(static_cast< AObj* >(ope1->Subtract(*ope2)));
+    }
+}
+
+void Script::do_dump()
+{
+    while (!stack_.empty())
+    {
+        stack_.pop();
+    }
+}
+
+void Script::do_pop()
+{
+    if (!stack_.empty())
+    {
+        stack_.pop();
+    }
+}
+
+void Script::do_assert()
+{
+    AObj* arg = factory_.creat(instru_.argument, instru_.value);
+
+    if (!arg)
+    {
+        error_ = unknow_type;
+    }
+    if (!(static_cast<IObject*>(stack_.top())->Equals(*arg)))
+    {
+        error_ = false_assert;
     }
 }
